a85d2a0dd18384f144cb0f0b41bf3022c9b87734
Matt Rudge <matt.rudge@codeinstitute.net>
2021-04-08 14:57:03 +0000

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
-# gitpod_migrate
-Migration tool for our template
+![CI logo](https://codeinstitute.s3.amazonaws.com/fullstack/ci_logo_small.png)
+# Arctic Tern
+
+## What?
+
+It's a migration tool for Gitpod. When students change their default editor in account settings to VSCode instead of Theia, certain parts of their older repositories may stop working.
+
+This tool can be provided to students. They will need to run it from their project directory.
+
+## How?
+
+Effectively, the tool simply downloads the latest settings files from our Gitpod Full Template repo, overwrites the existing ones and then renames the directory from `.theia` to `.vscode`.
+
+It also backs up the changed files by giving them a `.bak` suffix, though this can be switched off by running it with the `--nobackup` switch.
+
+## Usage
+
+`python3 arctictern.py [--nobackup]`
+
+The `--nobackup` switch will prevent backup files from being created.
+
+## Arctic Tern?
+
+It's a little bird that makes a big migration.
+
+I'll get my coat.
+
+------
+
+Matt Rudge<br/>
+April, 2021
---
+++ arctictern.py
@@ -0,0 +1,62 @@
+import os
+import requests
+import shutil
+import sys
+
+BASE_URL = "https://raw.githubusercontent.com/Code-Institute-Org/gitpod-full-template/master/"
+
+BACKUP = False
+
+FILE_LIST = [{  "filename": ".theia/settings.json",
+                "url": ".vscode/settings.json" },
+             {  "filename": ".gitpod.yml",
+                "url": ".gitpod.yml"
+             },
+             {  "filename": ".gitpod.dockerfile",
+                "url": ".gitpod.dockerfile"
+             }]
+
+
+def process(file, suffix):
+    """
+    Replaces and optionally backs up the files that
+    need to be changed.
+    Arguments: file - a path and filename
+               suffix - the suffix to the BASE_URL
+    """
+
+    if BACKUP:
+        shutil.copyfile(file, f"{file}.bak")
+
+    with open(file, "wb") as f:
+        r = requests.get(BASE_URL + suffix)
+        f.write(r.content)
+
+
+def start_migration():
+    """
+    Calls the process function and
+    renames the directory
+    """
+    if os.path.isdir(".theia"):
+        for file in FILE_LIST:
+            print(f"Processing: {file['filename']}")
+            process(file["filename"], file["url"])
+        print("Renaming directory")
+        os.rename(".theia", ".vscode")
+        print("Changes saved.")
+        print("Please add, commit and push to GitHub.")
+    else:
+        sys.exit("The .theia directory does not exist")
+
+
+if __name__ == "__main__":
+    print("CI Theia to VSCode Migration Utility")
+    print(f"Usage: python3 {sys.argv[0]} [--nobackup]")
+    print("If the --nobackup switch is provided, then changed files will not be backed up.")
+    print()
+    BACKUP = "--nobackup" in sys.argv
+    if input("Start migration? Y/N ").lower() == "y":
+        start_migration()
+    else:
+        sys.exit("Migration cancelled by the user")
