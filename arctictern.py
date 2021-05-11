3ae73a93ab033b59cf10b6f25afb6e96e934c53a
Matt Rudge <matt.rudge@codeinstitute.net>
2021-05-11 08:23:40 +0000

    add upgrade function

--- arctictern.py
+++ arctictern.py
@@ -11,15 +11,39 @@ import sys
 BASE_URL = "https://raw.githubusercontent.com/Code-Institute-Org/gitpod-full-template/master/"
 
 BACKUP = False
+UPGRADE = False
 
-FILE_LIST = [{"filename": ".theia/settings.json",
-              "url": ".vscode/settings.json"},
-             {"filename": ".gitpod.yml",
-              "url": ".gitpod.yml"
-              },
-             {"filename": ".gitpod.dockerfile",
-              "url": ".gitpod.dockerfile"
-              }]
+MIGRATE_FILE_LIST = [{"filename": ".theia/settings.json",
+                      "url": ".vscode/settings.json"
+                      },
+                     {"filename": ".gitpod.yml",
+                      "url": ".gitpod.yml"
+                      },
+                     {"filename": ".gitpod.dockerfile",
+                      "url": ".gitpod.dockerfile"
+                      },
+                     {"filename": ".theia/heroku_config.sh",
+                      "url": ".vscode/heroku_config.sh"
+                      },
+                     {"filename": ".theia/init_tasks.sh",
+                      "url": ".vscode/init_tasks.sh"
+                      }]
+
+UPGRADE_FILE_LIST = [{"filename": ".vscode/settings.json",
+                      "url": ".vscode/settings.json"
+                      },
+                     {"filename": ".gitpod.yml",
+                      "url": ".gitpod.yml"
+                      },
+                     {"filename": ".gitpod.dockerfile",
+                      "url": ".gitpod.dockerfile"
+                      },
+                     {"filename": ".vscode/heroku_config.sh",
+                      "url": ".vscode/heroku_config.sh"
+                      },
+                     {"filename": ".vscode/init_tasks.sh",
+                      "url": ".vscode/init_tasks.sh"
+                      }]
 
 
 def process(file, suffix):
@@ -31,7 +55,10 @@ def process(file, suffix):
     """
 
     if BACKUP:
-        shutil.copyfile(file, f"{file}.bak")
+        try:
+            shutil.copyfile(file, f"{file}.bak")
+        except FileNotFoundError:
+            print(f"{file} not found, a new one will be created")
 
     with open(file, "wb") as f:
         r = requests.get(BASE_URL + suffix)
@@ -43,25 +70,31 @@ def start_migration():
     Calls the process function and
     renames the directory
     """
-    if not os.path.isdir(".theia"):
+    if not os.path.isdir(".theia") and not UPGRADE:
         sys.exit("The .theia directory does not exist")
-    
+
+    FILE_LIST = UPGRADE_FILE_LIST if do_upgrade else MIGRATE_FILE_LIST
+
     for file in FILE_LIST:
         print(f"Processing: {file['filename']}")
         process(file["filename"], file["url"])
-        
-    print("Renaming directory")
-    os.rename(".theia", ".vscode")
+
+    if not UPGRADE:
+        print("Renaming directory")
+        os.rename(".theia", ".vscode")
+
     print("Changes saved.")
     print("Please add, commit and push to GitHub.")
-        
+
 
 if __name__ == "__main__":
     print("CI Theia to VSCode Migration Utility")
-    print(f"Usage: python3 {sys.argv[0]} [--nobackup]")
+    print(f"Usage: python3 {sys.argv[0]} [--nobackup --upgrade]")
     print("If the --nobackup switch is provided, then changed files will not be backed up.")
+    print("If the --upgrade switch is provided, the repo will be updated to the latest version of the template")
     print()
     BACKUP = "--nobackup" in sys.argv
+    UPGRADE = "--upgrade" in sys.argv
     if input("Start migration? Y/N ").lower() == "y":
         start_migration()
     else:
